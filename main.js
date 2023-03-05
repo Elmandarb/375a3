5aa742efdeaf1b2a4cef6f2765968c5bf4d0bf32
Elmandarb <bys.o.my@gmail.com>
2023-03-05 14:14:39 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+var canvas = undefined;
+var gl = undefined;
+var time = 0.0;
+
+    function init() {
+        canvas = document.getElementById("canvas");
+        gl = canvas.getContext("webgl2");
+
+        gl.clearColor(0.1, 0.1, 0.1, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+
+        cube = new Cube(gl);
+
+        cube.P = mat4();
+        cube.MV = mat4();
+
+        render();
+    }
+
+    function render() {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        time += 1.0;
+
+        cube.MV = rotateY(time);
+        
+
+        cube.render();
+
+        requestAnimationFrame(render);
+    }
+
+    window.onload = init;
